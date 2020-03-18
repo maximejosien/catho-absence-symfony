d4930fe844ef1e7ad79b6c8002a4f7821169a7bd
Rémi Marquilly <r.marquilly@gmail.com>
2020-03-18 09:13:51 +0100

    Absence type ENUM
    Ajout du type d'absence pour l'agenda

---
+++ src/Entity/AbsenceType.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Entity;
+
+/**
+ * L'entité a été créée de cette façon pour que l'autocomplétion fonctionne sur ces valeurs.
+ * Plus facile d'edit et de maintenir de cette façon.
+ * Appel de l'enum : PostStatus::VALEUR();
+ * @method static self CONGE_PAYE()
+ * @method static self TRAVAIL()
+ * @method static self FERIE()
+ * @method static self MALADIE()
+ * @method static self RTT()
+ *
+ */
+class AbsenceType
+{
+}
