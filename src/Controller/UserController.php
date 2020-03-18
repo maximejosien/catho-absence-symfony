c8173f706d0f47eba324eca01a9f5e2d3fbdd53a
Rémi Marquilly <r.marquilly@gmail.com>
2020-03-18 11:32:58 +0100

    Start UserList
    List of users to show (Needs a controller)

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class UserController extends AbstractController
+{
+ /**
+  * @Route("/Users", name = "app_users")
+  *
+  *
+  */
+
+ public function getAllUsers(){
+
+ }
+}
