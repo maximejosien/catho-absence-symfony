6dced859f0c07e07528c6dd24dba7e4fb9b40763
Maxime Josien <maxime@disruptual.com>
2020-03-27 09:11:33 +0100

    fix(calendar) update label in absence form

--- src/Form/AbsenceFormType.php
+++ src/Form/AbsenceFormType.php
@@ -8,18 +8,30 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AbsenceFormType extends AbstractType
 {
+    const ABSENCE = 'absence';
+    const DATE_FORM = 'DATE_FORM';
+    const HALF_DAY_TYPE_FORM = 'HALF_DAY_TYPE_FORM';
+    const ABSENCE_REASON_FORM = 'ABSENCE_REASON_FORM';
+    const SAVE_FORM = 'SAVE_FORM';
+
     /** @var AbsenceServiceInterface */
     private $absenceService;
 
+    /** @var TranslatorInterface */
+    private $translator;
+
     /**
      * @param AbsenceServiceInterface $absenceService
+     * @param TranslatorInterface $translator
      */
-    public function __construct(AbsenceServiceInterface $absenceService)
+    public function __construct(AbsenceServiceInterface $absenceService, TranslatorInterface $translator)
     {
         $this->absenceService = $absenceService;
+        $this->translator = $translator;
     }
 
     /**
@@ -29,13 +41,19 @@ class AbsenceFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dayAt', DateTimeType::class)
+            ->add('dayAt', DateTimeType::class, [
+                'label' => $this->translator->trans(self::DATE_FORM, [], self::ABSENCE),
+            ])
             ->add('halfDayType', ChoiceType::class, [
-                'choices' => $this->absenceService->getHalfDayTypes()
+                'label' => $this->translator->trans(self::HALF_DAY_TYPE_FORM, [], self::ABSENCE),
+                'choices' => $this->absenceService->getHalfDayTypes(),
             ])
             ->add('absenceReason', ChoiceType::class, [
-                'choices' => $this->absenceService->getAbsenceReasons()
+                'label' => $this->translator->trans(self::ABSENCE_REASON_FORM, [], self::ABSENCE),
+                'choices' => $this->absenceService->getAbsenceReasons(),
             ])
-            ->add('save', SubmitType::class);
+            ->add('save', SubmitType::class, [
+                'label' => $this->translator->trans(self::SAVE_FORM, [], self::ABSENCE),
+            ]);
     }
 }
