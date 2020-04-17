901ffbb2e68c168c3482264f3e98f95b80eeec3d
Maxime Josien <maxime@disruptual.com>
2020-04-17 22:18:23 +0200

    Links url correctly

--- src/Controller/ERPController.php
+++ src/Controller/ERPController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ERPController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/", name="app_home")
      *
      * @return Response
      */
-    public function home()
+    public function home(Security $security)
     {
-        return $this->render('home/home.html.twig');
+        $user = $security->getUser();
+
+        if ($user instanceof User) {
+            return $this->redirectToRoute('app_calendar', [
+                'dateFormat' => (new \DateTime())->format('F-Y')
+            ]);
+        }
+
+        return $this->redirectToRoute('app_login');
     }
 }
