afcf5c8306b194eb2fc97a1e289bd63c231a90df
hichem <hichembedjaoui4@gmail.com>
2020-06-03 22:32:28 +0200

    feat(pdf): wip

---
+++ src/Controller/PdfGenerator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PdfGenerator extends AbstractController
+{
+
+    /**
+     * @Route("/pdf", name="app_pdf")
+     */
+    public function generate_pdf()
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('calendar/calendar.html.twig', [
+            'title' => "Votre Export"
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => true
+        ]);
+    }
+}
